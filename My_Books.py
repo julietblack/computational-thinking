b4f425ad22789cca659c8a9b5ade62915ba50c98
Juliet Black <43437347+julietblack@users.noreply.github.com>
2018-10-12 14:12:09 +0200

    Update My_Books.py

--- My_Books.py
+++ My_Books.py
@@ -2,11 +2,11 @@ import Book
 
 import Q_Sort     
 
-I_Am_Number_Four = Book.Book('I Am Number Four', Book.Author('Pittacus', 'Lore'), '444', '9780061969577')
+I_Am_Number_Four = Book.Book('I Am Number Four', Book.Author('Pittacus', 'Lore'), 444, '9780061969577')
 
-Bring_the_Heat = Book.Book('Bring the Heat', Book.Author('G.A.', 'Aiken'), '416', '142013163X')
+Bring_the_Heat = Book.Book('Bring the Heat', Book.Author('G.A.', 'Aiken'), 416, '142013163X')
 
-The_Giver = Book.Book('The Giver', Book.Author('Lois', 'Lowry'), '204', '0544336259')
+The_Giver = Book.Book('The Giver', Book.Author('Lois', 'Lowry'), 204, '0544336259')
 
 books = [I_Am_Number_Four, Bring_the_Heat, The_Giver]
 Q_Sort.quicksort(books)
