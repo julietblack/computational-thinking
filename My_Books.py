2c595b6d9bab8a19150bfe5aedd150476f9b3ec5
Juliet Black <43437347+julietblack@users.noreply.github.com>
2018-10-12 11:42:29 +0200

    Add files via upload

---
+++ My_Books.py
@@ -0,0 +1,14 @@
+import Book 
+
+import Q_Sort     
+
+I_Am_Number_Four = Book.Book('I Am Number Four', Book.Author('Pittacus', 'Lore'), '444', '9780061969577')
+
+Bring_the_Heat = Book.Book('Bring the Heat', Book.Author('G.A.', 'Aiken'), '416', '142013163X')
+
+The_Giver = Book.Book('The Giver', Book.Author('Lois', 'Lowry'), '204', '0544336259')
+
+books = [I_Am_Number_Four, Bring_the_Heat, The_Giver]
+Q_Sort.quicksort(books)
+for n in books:
+	print(str(n) + '\n') 
