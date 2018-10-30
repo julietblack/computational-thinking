de1b4390779b364e53e7489aecf0a9941656fa47
Juliet Black <43437347+julietblack@users.noreply.github.com>
2018-10-30 14:01:51 +0100

    Add files via upload

--- Find_Pairs.py
+++ Find_Pairs.py
@@ -2,12 +2,18 @@ a = [3, 4, 1, 7, 9, 17]
 x = 8
 
 def Find_Pairs(a, x):
-    for x in a:
-        for i in range(0, n): 
-        for j in range(i + 1, n): 
-            if arr[i] + arr[j] == sum: 
-                count += 1
-		return True
-    return False
+    i = 0
+    j = len(a) - 1
+    while i < j:
+        if a[i] + a[j] == x:
+            return (i, j)
+        if a[i] + a[j] < x:
+            i += 1
+        else:
+            j -= 1
+    return None
+
+
+print(Find_Pairs(x, 21))
 
 #I'm sorry this is not complete. I promise I did my best and I smiled at the computer. 
